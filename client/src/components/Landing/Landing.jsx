31fdbcfd85dc328499a3090777e91782f8f36832
Kikepal88 <omarpal88@gmail.com>
2023-03-09 00:32:04 -0500

    Se crean estilos para hacer responsive el landing, se arreglan algunas cosas de estilos generales para mejorar el responsive pero se deben arreglar aun temas del main y el BG

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -9,7 +9,7 @@ export default function Landing() {
         <h3>A video game library where you can find information about your favorite titles, or those you want to get.</h3>
         <h4> You will be able to see their description, rating, platforms where they can be played and much more. Enter now and start looking for your games.</h4>
         <Link to={'/home'}>
-          <button>Start</button>
+          <button>START</button>
         </Link>
       </section>
     </main>
